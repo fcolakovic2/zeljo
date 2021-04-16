5c21d473153c89401e16b2d0f5c642f30635f542
Faris Čolaković <fcolakovic2@etf.unsa.ba>
2021-04-16 15:25:09 +0200

    Napravljena pocetni novi dart fajl za prikaz screena za sign in to jest home pagea, main.dart izbrisan i samo poziva ovaj sign in screen za testiranje trenutno

---
+++ lib/view/homePageView/pages/signInScreen.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
