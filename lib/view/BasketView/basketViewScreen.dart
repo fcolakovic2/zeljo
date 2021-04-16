2d6f246f7fbb4aee14a372074d8ce7e0eb21fa8e
ilicina1 <ilhan.licina@outlook.com>
2021-04-16 14:47:45 +0200

    dodata funkcionalnost addorder

---
+++ lib/view/BasketView/basketViewScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BasketScreen extends StatefulWidget {
+  @override
+  _BasketScreenState createState() => _BasketScreenState();
+}
+
+class _BasketScreenState extends State<BasketScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
