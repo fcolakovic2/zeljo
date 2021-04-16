d8bdc470beb513a6956d12f87d2d07a9cc84a3ff
Faris Čolaković <fcolakovic2@etf.unsa.ba>
2021-04-16 15:12:08 +0200

    Napravljen custom displej za greske prilikom sign ina, incijaliiziran firebase

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -1,1 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
+class Authentication {
+  static SnackBar displaySnackBar({required String content}) {
+    return SnackBar(
+      backgroundColor: Colors.black,
+      content: Text(
+        content,
+        style: TextStyle(color: Colors.grey, letterSpacing: 0.2),
+      ),
+    );
+  }
+
+  static Future<FirebaseApp> initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+
+    return firebaseApp; //mora se uvijek firebase inicijalizirati
+  }
+}
