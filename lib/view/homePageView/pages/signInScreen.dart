eb9d177215705293fa3a79377ff571464718f787
Faris Čolaković <fcolakovic2@etf.unsa.ba>
2021-04-17 15:21:48 +0200

    Ubacene slike ispravno nakon problema sa importom asseta, ostalo stilizirati dugme

--- lib/view/homePageView/pages/signInScreen.dart
+++ lib/view/homePageView/pages/signInScreen.dart
@@ -12,68 +12,79 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.orange,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            left: 16.0,
-            right: 16.0,
-            bottom: 20.0,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Row(),
-              Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Flexible(
-                      flex: 1,
-                      child: Image.asset(
-                        'assets/drawable-hdpi/zeljoLogo.png',
-                        height: 160,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      'Zeljo',
-                      style: TextStyle(
-                        color: Colors.yellow,
-                        fontSize: 40,
-                      ),
-                    ),
-                    Text(
-                      'Authentication',
-                      style: TextStyle(
-                        color: Colors.orange,
-                        fontSize: 40,
-                      ),
-                    ),
-                  ],
-                ),
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          'assets/images/login_background.png',
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 16.0,
+                right: 16.0,
+                bottom: 20.0,
               ),
-              FutureBuilder(
-                future: Authentication.initializeFirebase(context: context),
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return Text('Error prilikom authenticationa');
-                  } else if (snapshot.connectionState == ConnectionState.done) {
-                    return GoogleSignInButton();
-                  }
-                  return CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(
-                      Colors.orange,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Row(),
+                  Expanded(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Flexible(
+                          flex: 1,
+                          child: Image.asset(
+                            'assets/drawable-hdpi/zeljoLogo.png',
+                            height: 160,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          'Željo',
+                          style: TextStyle(
+                            color: Colors.yellow,
+                            fontSize: 40,
+                          ),
+                        ),
+                        Text(
+                          'Authentication',
+                          style: TextStyle(
+                            color: Colors.orange,
+                            fontSize: 40,
+                          ),
+                        ),
+                      ],
                     ),
-                  );
-                },
+                  ),
+                  FutureBuilder(
+                    future: Authentication.initializeFirebase(context: context),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return Text('Error prilikom authenticationa');
+                      } else if (snapshot.connectionState ==
+                          ConnectionState.done) {
+                        return GoogleSignInButton();
+                      }
+                      return CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(
+                          Colors.orange,
+                        ),
+                      );
+                    },
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
