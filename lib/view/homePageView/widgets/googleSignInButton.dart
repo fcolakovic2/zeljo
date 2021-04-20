ebd58cd3d970b94672e0ccf1814e8c904564a491
ilicina1 <ilhan.licina@outlook.com>
2021-04-20 09:10:27 +0200

    izmjena naziva fajlova

--- lib/view/homePageView/widgets/googleSignInButton.dart
+++ lib/view/homePageView/widgets/googleSignInButton.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:zeljoprojekat/utils/shared/sizeConfig.dart';
+import 'package:zeljoprojekat/utils/shared/size_config.dart';
 import 'package:zeljoprojekat/utils/style/style.dart';
 import 'package:zeljoprojekat/view/homePageView/widgets/googleLogo.dart';
 import 'package:zeljoprojekat/view/homePageView/widgets/googleTekst.dart';
