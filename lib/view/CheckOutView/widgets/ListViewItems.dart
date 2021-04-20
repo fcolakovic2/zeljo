531368a9d9527fb4b53e914fafd3a8236ca49e11
ilicina1 <ilhan.licina@outlook.com>
2021-04-20 10:22:06 +0200

    sitne izmjene pred push na main

--- lib/view/CheckOutView/widgets/ListViewItems.dart
+++ lib/view/CheckOutView/widgets/ListViewItems.dart
@@ -13,7 +13,6 @@ class ListViewItems extends StatefulWidget {
 class _ListViewItemsState extends State<ListViewItems> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
   }
 
--- lib/view/mealDetailsView/pages/mealDetailsView.dart
+++ lib/view/mealDetailsView/pages/mealDetailsView.dart
@@ -16,7 +16,6 @@ class MealDetails extends StatefulWidget {
 class _MealDetailsState extends State<MealDetails> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
   }
 
