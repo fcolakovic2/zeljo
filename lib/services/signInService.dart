931be57073bc9a3f35be272d4839c2f45a290d30
Ajla Dzaferbegovic <56268385+adzaferbeg1@users.noreply.github.com>
2021-04-21 10:44:02 +0200

    ispravka importa

--- lib/services/signInService.dart
+++ lib/services/signInService.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:zeljoprojekat/interface/signInInterface.dart';
+import 'package:zeljoprojekat/interface/signingInterface/signInInterface.dart';
 import 'package:zeljoprojekat/navigationBar.dart';
 import 'package:zeljoprojekat/utils/style/style.dart';
 
